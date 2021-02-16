09ceb11c154af0574249a1e5eb49fae9c5cec763
Vebjoern Vaardal <Vebjoern Vaardal@DESKTOP-VI6RK0G>
2021-02-16 14:28:15 +0100

    Swag

--- DAT109Oblig2/src/no/hvl/dat109/Bilutleieselskap.java
+++ DAT109Oblig2/src/no/hvl/dat109/Bilutleieselskap.java
@@ -1,5 +1,14 @@
 package no.hvl.dat109;
 
+import java.util.List;
+
 public class Bilutleieselskap {
 
+	private String Navn;
+	private int TelefonNr;
+	private String Adresse;
+	
+	List<Utleiekontor> kontorer;
+	List<Kunde> kunder;
+	
 }
--- DAT109Oblig2/src/no/hvl/dat109/Utleiekontor.java
+++ DAT109Oblig2/src/no/hvl/dat109/Utleiekontor.java
@@ -2,4 +2,6 @@ package no.hvl.dat109;
 
 public class Utleiekontor {
 
+	private int Nummer;
+	
 }
