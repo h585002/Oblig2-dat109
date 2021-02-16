2f075c67a81abc1279f09b88c412563a65622c1c
h585002 <54101409+h585002@users.noreply.github.com>
2021-02-16 12:40:02 +0100

    Add files via upload

---
+++ DAT109-OBLIG-2/src/no/hvl/dat109/Utleiekontor.java
@@ -0,0 +1,17 @@
+package no.hvl.dat109;
+
+public class Utleiekontor {
+	
+	int kontorNummer;
+	int telefonNummer;
+	Adresse adresse;
+	
+	public Utleiekontor(int kontorNummer, int telefonNummer, Adresse adresse) {
+		this.kontorNummer = kontorNummer;
+		this.telefonNummer = telefonNummer;
+		this.adresse = adresse;
+	}
+	
+	
+
+}
