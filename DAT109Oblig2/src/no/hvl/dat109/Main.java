d00f1d8c9020a8cda313a64b29a7124017fba8e9
Vebjoern Vaardal <Vebjoern Vaardal@DESKTOP-VI6RK0G>
2021-02-18 23:24:15 +0100

    Formatert

--- DAT109Oblig2/src/no/hvl/dat109/Main.java
+++ DAT109Oblig2/src/no/hvl/dat109/Main.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 
 public class Main {
 
-	//Midlertidig main, bare å endre på ting
-	//Vil vi gjøre alt med scanner?
+	// Midlertidig main, bare å endre på ting
+	// Vil vi gjøre alt med scanner?
 	public static void main(String[] args) {
-		
-		//Later som vi henter eksisterende kunder, kontorer og biler fra database, istedenfor å implementere det.
+
+		// Later som vi henter eksisterende kunder, kontorer og biler fra database,
+		// istedenfor å implementere det.
 		Database database = new Database();
 		Bilutleieselskap bilutleieselskap = database.hentInfo();
-		
-		Scanner sc = new Scanner(System.in);	
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Velkommen til Bonanza Bilutleie!" + "\n" + "\n" + "Skriv inn dine brukeropplysninger:");
-		
-		//Bør kanskje ha validering på alt
+
+		// Bør kanskje ha validering på alt
 		System.out.println("Fornavn:");
 		String fornavn = sc.nextLine();
 		System.out.println("Etternavn:");
@@ -25,12 +26,12 @@ public class Main {
 		String adresse = sc.nextLine();
 		System.out.println("TelefonNummer:");
 		int telefonNr = sc.nextInt();
-		
+
 		Kunde kunde = new Kunde(fornavn, etternavn, adresse, telefonNr, new ArrayList<Reservasjon>());
 		System.out.println("Stemmer disse opplysningene om deg?" + "\n" + kunde.toString());
-		
+
 		sc.close();
-		
+
 	}
 
 }
