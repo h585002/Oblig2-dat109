c894dc9ab6748d06a143ddf87d62bb6060db8ea3
oyste <oyste@192.168.10.105>
2021-02-22 20:30:34 +0100

    tester for alle validator metoder

--- DAT109Oblig2/src/test/ValidatorTest.java
+++ DAT109Oblig2/src/test/ValidatorTest.java
@@ -5,7 +5,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import object.Bilutleieselskap;
+import object.Database;
+
 public class ValidatorTest {
+	
+	static Database database = new Database();
+	static Bilutleieselskap bilutleieselskap = database.hentInfo();
 
 	public Validator validator = new Validator();
 	
@@ -28,5 +34,70 @@ public class ValidatorTest {
 	@Test
 	public void adresseTrue() {
 		assertTrue(validator.adresseSjekk("Eikelandsbakken 23"));
+	}
+	
+	@Test
+	public void TelefonNrTrue() {
+		assertTrue(validator.telefonNrSjekk("34343434", bilutleieselskap));
+	}
+	
+	@Test
+	public void TelefonNrFalse() {
+		assertFalse(validator.telefonNrSjekk("23", bilutleieselskap));
+	}
+	
+	@Test
+	public void kredittkortTrue() {
+		assertTrue(validator.kredittkortNrSjekk("1122334455667788"));
+	}
+	
+	@Test
+	public void kontorTrue() {
+		assertTrue(validator.kontorSjekk("Oslo", bilutleieselskap));
+	}
+	
+	@Test
+	public void kontorFalse() {
+		assertFalse(validator.kontorSjekk("Osl", bilutleieselskap));
+	}
+	
+	@Test
+	public void aarTrue() {
+		assertTrue(validator.aarSjekk("2021"));
+	}
+	
+	@Test
+	public void maanedTrue() {
+		assertTrue(validator.maanedSjekk("02", 2021));
+	}
+	
+	@Test
+	public void dagFalse() {
+		assertFalse(validator.dagSjekk("88", 2021, 02));
+	}
+	
+	@Test
+	public void timeFalse() {
+		assertFalse(validator.timeSjekk("34"));
+	}
+	
+	@Test
+	public void minuttTrue() {
+		assertTrue(validator.minuttSjekk("15"));
+	}
+	
+	@Test
+	public void dagerFalse() {
+		assertFalse(validator.dagerSjekk("-1"));
+	}
+	
+	@Test
+	public void kategoriTrue() {
+		assertTrue(validator.kategoriSjekk("A", true, false, false, false));
+	}
+	
+	@Test
+	public void kategoriFalse() {
+		assertFalse(validator.kategoriSjekk("b", true, false, false, false));
 	}
 }
